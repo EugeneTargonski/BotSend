ee90d493472a13b3c49014c36b2b41f7289e5db7
eugene_targonski <tevstim@gmail.com>
2020-03-20 13:08:59 +0300

    initial

---
+++ BotSend/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BotSend
+{
+    static class Program
+    {
+        static async Task SendMessage(string message)
+        {
+            var AccessToken = ConfigurationManager.AppSettings.Get("AccessToken");
+            var ChanellName = ConfigurationManager.AppSettings.Get("ChanellName");
+
+            var bot = new TelegramBotClient(AccessToken);
+            _ = bot.GetMeAsync().Result;
+            await bot.SendTextMessageAsync(ChanellName, message);
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a message text.");
+            }
+            else
+            {
+                var task = SendMessage(args[0]);
+                task.Wait();
+            }
+        }
+    }
+}
